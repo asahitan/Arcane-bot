8492d17e6c09fed97758a4c3ed295ca5e39c411b
Asahi <163096912+asahitan@users.noreply.github.com>
2025-05-12 08:59:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('add-to-discord').addEventListener('click', function() {
-    window.open('https://discord.com/oauth2/authorize?client_id=1096950687826448407&permissions=8&integration_type=0&scope=bot+applications.commands', '_blank');
+    window.open('https://discord.com/oauth2/authorize?client_id=1091852954069381200', '_blank');
 });
